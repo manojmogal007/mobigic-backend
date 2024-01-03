55718389183f88e56d2b711a146cf0cc29164104
manojmogal007 <manojmogal07@gmail.com>
2024-01-03 22:50:26 +0530

    update

--- index.js
+++ index.js
@@ -7,12 +7,6 @@ const fs = require('fs').promises
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
 
 app.use(cors({
   origin:"*"
