4bf497a9186ac066de5fea6a499b21024efbeac6
manojmogal007 <manojmogal07@gmail.com>
2024-01-03 19:55:47 +0530

    update

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const cors=require('cors')
 const fs = require('fs').promises
 
 const app = express();
-app.use(cors())
+app.use(cors({
+  origin:"*"
+}))
 app.use(express.json())
 const PORT = process.env.PORT || 3000;
 
